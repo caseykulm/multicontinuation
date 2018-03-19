a649e68a880e11f4c28ef8bfbe0f1068cdc2df11
Casey Kulm <mlukyesac@gmail.com>
2018-03-18 22:01:42 -0600

    Using example

---
+++ src/main/java/com/caseykulm/intellij/multicontinuation/MultiContinuationIndentAction.kt
@@ -0,0 +1,10 @@
+package com.caseykulm.intellij.multicontinuation
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class MultiContinuationIndentAction : AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+
+  }
+}
